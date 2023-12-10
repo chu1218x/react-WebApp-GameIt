c747cc4eb374f6ed46ab8c65f60d61ae50837400
xuanchu jiang <xuanchujiang@gmail.com>
2023-12-10 15:37:59 -0500

    changed back /project

--- src/App.js
+++ src/App.js
@@ -2,35 +2,18 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Project from './project';
-import Home from './project/home';
-import SignIn from './project/users/signin';
-import SignUp from './project/signup';
-import Account from './project/users/account';
-import GameList from './project/gamelist';
-import Search from './project/search';
-import Details from './project/details';
-import UserList from './project/users/list';
-import UserDetails from './project/users/details';
 
 function App() {
+
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Project><Home /></Project>} />
-        <Route path="/home" element={<Project><Home /></Project>} />
-        <Route path="/signin" element={<Project><SignIn /></Project>} />
-        <Route path="/signup" element={<Project><SignUp /></Project>} />
-        <Route path="/account" element={<Project><Account /></Project>} />
-        <Route path="/gamelist" element={<Project><GameList /></Project>} />
-        <Route path="/search" element={<Project><Search /></Project>} />
-        <Route path="/search/:searchTerm" element={<Project><Search /></Project>} />
-        <Route path="/details/:gameId" element={<Project><Details /></Project>} />
-        <Route path="/users" element={<Project><UserList /></Project>} />
-        <Route path="/user/:userId" element={<Project><UserDetails /></Project>} />
-        {/* 可以根据需要添加更多路由 */}
+        <Route path="/" element={<Navigate to="project" />} />
+        <Route path="/project/*"  element={<Project/>}/>
       </Routes>
     </Router>
-  );
+  )
 }
 
 export default App;
--- src/project/index.js
+++ src/project/index.js
@@ -1,30 +1,52 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
+import Signup from './signup';
+import Account from './users/account';
+import GameList from './gamelist';
+import Search from './search';
+import Deatils from './details';
+import UserList from './users/list';
+import UserDetails from './users/details';
+import Home from './home';
+import SignIn from './users/signin';
 
-function Project({ children }) {
+function Project() {
+
     return (
         <div className='container-fluid'>
             <h1>Game It</h1>
             <div className='row'>
                 <div className='col-2'>
                     <div className="list-group">
-                        <Link to="/home" className="list-group-item list-group-item-action" >
+                        <Link to="/project" className="list-group-item list-group-item-action" >
                             Home</Link>
-                        <Link to="/signin" className="list-group-item list-group-item-action" >
+                        <Link to="/project/signin" className="list-group-item list-group-item-action" >
                             Sign In</Link>
-                        <Link to="/signup" className="list-group-item list-group-item-action" >
+                        <Link to="/project/signup" className="list-group-item list-group-item-action" >
                             Sign up</Link>
-                        <Link to="/account" className="list-group-item list-group-item-action" >
+                        <Link to="/project/account" className="list-group-item list-group-item-action" >
                             Account</Link>                  
-                        <Link to="/gamelist" className="list-group-item list-group-item-action" >
+                        <Link to="/project/gamelist" className="list-group-item list-group-item-action" >
                             All Games</Link>
                     </div>
                 </div>
                 <div className='col-10'>
-                    {children}
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/signin" element={<SignIn />} />
+                        <Route path="/signup" element={<Signup />} />
+                        <Route path="/account" element={<Account />} />
+                        <Route path="/search" element={<Search />} />
+                        <Route path="/search/:search" element={<Search />} />
+                        <Route path="/details/:gameId" element={<Deatils />} />
+                        <Route path='/gamelist' element={<GameList />} />
+                        <Route path='/users' element={<UserList />} />
+                        <Route path='/user/:userId' element={<UserDetails />} />
+                    </Routes>
                 </div>
             </div>
         </div>
+
     );
 }
 
