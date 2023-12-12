8af29e6b6446f0187ab9334b39ca70195813bc8f
xuanchu jiang <xuanchujiang@gmail.com>
2023-12-12 16:47:28 -0500

    removed signUp creatorInfo

--- src/project/creators.js
+++ src/project/creators.js
@@ -2,27 +2,44 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import * as client from './client';
 import "../project/stylelist/creators.css";
-import axios from 'axios';
-import defaultImage from '../project/image/User_Avatar.png'; 
+// import axios from 'axios';
+// import defaultImage from '../project/image/User_Avatar.png'; 
 
 
 function Creators() {
+    // const [rawgCreators, setRawgCreators] = useState([]); 
+    // const [localCreators, setLocalCreators] = useState([]); 
+
+    // const fetchRawgCreators = async () => {
+    //     try {
+    //         const rawgData = await client.findAllCreators();
+    //         setRawgCreators(rawgData);
+    //     } catch (error) {
+    //         console.error("Error fetching RAWG creators:", error);
+    //     }
+    // };
+
+    // const fetchLocalCreators = async () => {
+    //     try {
+    //         const response = await axios.get('http://localhost:4000/api/creators');
+    //         setLocalCreators(response.data);
+    //     } catch (error) {
+    //         console.error("Error fetching local creators:", error);
+    //     }
+    // };
+
+    // useEffect(() => {
+    //     fetchRawgCreators();
+    //     fetchLocalCreators();
+    // }, []);
+
     const [creators, setCreators] = useState([]);
 
     useEffect(() => {
         const fetchCreators = async () => {
             try {
-                // fetch data from RAWG API
-                const rawgData = await client.findAllCreators();
-
-                // fetch data from our own API  
-                const response = await axios.get('http://localhost:4000/api/creators');
-                const ownCreators = response.data;
-
-                // combine the two arrays
-                const combinedCreators = [...rawgData, ...ownCreators];
-                
-                setCreators(combinedCreators);
+                const data = await client.findAllCreators();
+                setCreators(data);
             } catch (error) {
                 console.error("Error fetching creators:", error);
             }
@@ -31,15 +48,12 @@ function Creators() {
         fetchCreators();
     }, []);
 
-
     return (
         <div className="creators-container">
             {creators.map(creator => (
                 <div key={creator.id} className="creator-card">
                     <div className="creator-image-container">
-                        <img 
-                            src={creator.image || defaultImage} 
-                            alt={creator.name} className="creator-image" />
+                        <img src={creator.image} alt={creator.name} className="creator-image" />
                     </div>
                     <div className="creator-info">
                         <h2 className="creator-name">{creator.name}</h2>
@@ -57,14 +71,78 @@ function Creators() {
                                 <span>No games available</span>
                             )}
                         </p>
-
                     </div>
                 </div>
             ))}
         </div>
     );
-
 }
+//     return (
+//         <div className="creators-container">
+//             {/* from RAWG */}
+//             {rawgCreators.map(creator => (
+//                 <div key={creator.id} className="creator-card">
+//                     <div className="creator-image-container">
+//                         <img 
+//                             src={creator.image || defaultImage} 
+//                             alt={creator.name} 
+//                             className="creator-image" 
+//                         />
+//                     </div>
+//                     <div className="creator-info">
+//                         <h2 className="creator-name">{creator.name}</h2>
+//                         <p className="creator-games-count">Games Count: {creator.games_count}</p>
+//                         <p className="creator-position">Position: {creator.positions.map(position => position.name).join(', ')}</p>
+//                         <p className="creator-games">
+//                             Games:{' '}
+//                             {creator.games && creator.games.length > 0 ? (
+//                                 creator.games.map(game => (
+//                                     <Link key={game.id} to={`/project/details/${game.id}`} className="game-link">
+//                                         {game.name}
+//                                     </Link>
+//                                 )).reduce((prev, curr) => [prev, ', ', curr])
+//                             ) : (
+//                                 <span>No games available</span>
+//                             )}
+//                         </p>
+//                     </div>
+//                 </div>
+//             ))}
+    
+//             {/* from local */}
+//             {localCreators.map(creator => (
+//                 <div key={creator._id} className="creator-card">
+//                     <div className="creator-image-container">
+//                         <img 
+//                             src={creator.profileImage || defaultImage} 
+//                             alt={creator.name} 
+//                             className="creator-image" 
+//                         />
+//                     </div>
+//                     <div className="creator-info">
+//                         <h2 className="creator-name">{creator.name}</h2>
+//                         <p className="creator-games-count">Games Count: {creator.games_count}</p>
+//                         <p className="creator-position">Position: {creator.positions.map(position => position.name).join(', ')}</p>
+//                         <p className="creator-games">
+//                             Games:{' '}
+//                             {creator.games && creator.games.length > 0 ? (
+//                                 creator.games.map(game => (
+//                                     <Link key={game.id} to={`/project/details/${game.id}`} className="game-link">
+//                                         {game.name}
+//                                     </Link>
+//                                 )).reduce((prev, curr) => [prev, ', ', curr])
+//                             ) : (
+//                                 <span>No games available</span>
+//                             )}
+//                         </p>
+//                     </div>
+//                 </div>
+//             ))}
+//         </div>
+//     );
+    
+
+// }
 
 
 export default Creators;
--- src/project/users/signup.js
+++ src/project/users/signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import * as client from "./client";
+
 function Signup() {
     const navigate = useNavigate();
 
@@ -8,54 +9,20 @@ function Signup() {
     const [credentials, setCredentials] = useState({
         username: "",
         password: "",
-        role: "USER",
-        creatorInfo: { 
-            name: "",
-            games_count: 0,
-            positions: "",
-            games: ""
-        }
+        role: "USER"
     });
 
     const signup = async () => {
         try {
-            if (credentials.role === 'CREATOR') {
-                const positionsArray = credentials.creatorInfo.positions.split(',').map(pos => ({ name: pos.trim() }));
-                const gamesArray = credentials.creatorInfo.games.split(',').map(game => {
-                    const [name, id] = game.trim().split(':');
-                    return { name, id: id ? Number(id) : undefined };
-                });
-
-                const creatorCredentials = {
-                    ...credentials,
-                    creatorInfo: {
-                        ...credentials.creatorInfo,
-                        positions: positionsArray,
-                        games: gamesArray
-                    }
-                };
-
-                await client.signUp(creatorCredentials);
-            } else {
-                await client.signUp(credentials);
-            }
-
+            await client.signUp(credentials);
             navigate("/project/signin");
         } catch (err) {
             setError(err.response.data.message);
         }
     };
 
-
     const handleRoleChange = (event) => {
         setCredentials({ ...credentials, role: event.target.value });
-    };
-
-    const handleCreatorChange = (event) => {
-        setCredentials({
-            ...credentials,
-            creatorInfo: { ...credentials.creatorInfo, [event.target.name]: event.target.value }
-        });
     };
 
     return (
@@ -111,46 +78,11 @@ function Signup() {
                 </label>
             </div>
 
-            {credentials.role === "CREATOR" && (
-                <div>
-                    <p>Name:</p>
-                    <input
-                        type="text"
-                        name="name"
-                        value={credentials.creatorInfo.name}
-                        onChange={handleCreatorChange}
-                    />
-                    
-                    <p>Games Count:</p>
-                    <input
-                        type="number"
-                        name="games_count"
-                        value={credentials.creatorInfo.games_count}
-                        onChange={handleCreatorChange}
-                    />
-                    <p>Positions (comma-separated):</p>
-                    <input
-                        type="text"
-                        name="positions"
-                        value={credentials.creatorInfo.positions}
-                        onChange={handleCreatorChange}
-                    />
-                    <p>Games (comma-separated):</p>
-                    <input
-                        type="text"
-                        name="games"
-                        value={credentials.creatorInfo.games}
-                        onChange={handleCreatorChange}
-                    />
-                </div>
-            )}
-
             <button onClick={signup} className="btn btn-primary">
                 Signup
             </button>
         </div>
     );
-
-
 }
+
 export default Signup;
