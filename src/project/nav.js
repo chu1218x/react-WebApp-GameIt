614d5ff669373d56fa2a4e72bba18fbed555d3c8
Wenxi Zhang <zhang.wenxi1@northeastern.edu>
2023-12-12 21:54:43 -0500

    nav Dropdown added

--- src/project/nav.js
+++ src/project/nav.js
@@ -1,26 +1,44 @@
-import { Link, useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Dropdown } from 'react-bootstrap';
 import './stylelist/nav.css'
 
+
+
 function Nav() {
-    
     return (
-        <div className='nav-container col-2 d-none d-lg-block'>
-            <div className="list-group">
-                <Link to="/project" className="list-group-item list-group-item-action" >
-                    Home</Link>
-                <Link to="/project/signin" className="list-group-item list-group-item-action" >
-                    Sign In</Link>
-                <Link to="/project/signup" className="list-group-item list-group-item-action" >
-                    Sign up</Link>
-                {/* <Link to="/project/account" className="list-group-item list-group-item-action" >
+        <>
+            <div className='nav-container col-2 d-none d-lg-block'>
+                <div className="list-group">
+                    <Link to="/project" className="list-group-item list-group-item-action" >
+                        Home</Link>
+                    <Link to="/project/signin" className="list-group-item list-group-item-action" >
+                        Sign In</Link>
+                    <Link to="/project/signup" className="list-group-item list-group-item-action" >
+                        Sign up</Link>
+                    {/* <Link to="/project/account" className="list-group-item list-group-item-action" >
                     Account</Link> */}
-                <Link to="/project/gamelist" className="list-group-item list-group-item-action" >
-                    All Games</Link>
-                <Link to="/project/creators" className="list-group-item list-group-item-action" >
-                    Creators</Link>
+                    <Link to="/project/gamelist" className="list-group-item list-group-item-action" >
+                        All Games</Link>
+                    <Link to="/project/creators" className="list-group-item list-group-item-action" >
+                        Creators</Link>
+                </div>            </div>
+
+            <div className="dropdown-nav-menu d-lg-none">
+                <Dropdown>
+                    <Dropdown.Toggle style={{ backgroundColor: 'rgb(11, 142, 98)', color: 'white', borderColor: 'rgb(11, 142, 98)' }}>
+                        Menu
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item as={Link} to="/project">Home</Dropdown.Item>
+                        <Dropdown.Item as={Link} to="/project/signin">Sign In</Dropdown.Item>
+                        <Dropdown.Item as={Link} to="/project/signup">Sign up</Dropdown.Item>
+                        <Dropdown.Item as={Link} to="/project/gamelist">All Games</Dropdown.Item>
+                        <Dropdown.Item as={Link} to="/project/creators">Creators</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
             </div>
-        </div>
+        </>
     );
 }
 
